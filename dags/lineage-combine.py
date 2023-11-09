059e2471c1a413f8d15d2a15b17b3b906f3bfdc3
Nidhi Jain <nidhi@Nidhis-MacBook-Pro.local>
2023-11-09 16:03:05 +0530

    Adding a new task in lineage-combine

--- dags/lineage-combine.py
+++ dags/lineage-combine.py
@@ -3,8 +3,27 @@ import datetime
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-# create_pet_table, populate_pet_table, get_all_pets, and get_birth_date are examples of tasks created by
-# instantiating the Postgres Operator
+create_base_tables_query = """
+CREATE TABLE IF NOT EXISTS adoption_center_1
+(date DATE, type VARCHAR, name VARCHAR, age INTEGER);
+
+CREATE TABLE IF NOT EXISTS adoption_center_2
+(date DATE, type VARCHAR, name VARCHAR, age INTEGER);
+
+INSERT INTO
+    adoption_center_1 (date, type, name, age)
+VALUES
+    ('2022-01-01', 'Dog', 'Bingo', 4),
+    ('2022-02-02', 'Cat', 'Bob', 7),
+    ('2022-03-04', 'Fish', 'Bubbles', 2);
+
+INSERT INTO
+    adoption_center_2 (date, type, name, age)
+VALUES
+    ('2022-06-10', 'Horse', 'Seabiscuit', 4),
+    ('2022-07-15', 'Snake', 'Stripes', 8),
+    ('2022-08-07', 'Rabbit', 'Hops', 3);
+"""
 
 with DAG(
     dag_id="lineage_combine",
@@ -12,6 +31,11 @@ with DAG(
     schedule_interval="0 0 * * *",
     catchup=False,
 ) as dag:
+    create_base__table = create_table = PostgresOperator(
+        task_id="create_table",
+        postgres_conn_id='postgres-default',
+        sql=create_base_tables_query,
+    )
     create_table = PostgresOperator(
         task_id="create_table",
         postgres_conn_id='postgres-default',
@@ -36,4 +60,4 @@ with DAG(
         FROM adoption_center_2;
         """
     ) 
-    create_table >> insert_data
+    create_base__table >> create_table >> insert_data
